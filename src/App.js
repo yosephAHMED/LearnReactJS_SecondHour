5e7ea7c6070620b816888e5af60590409c67d4cd
YosephAhmed <yosephahmed@aol.com>
2020-06-03 22:46:14 -0400

    Added a ContactCard component and rendered it in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import ContactCard from "./components/ContactCard"
 
 function App() {
   return (
     <div>
-      
+      <ContactCard />
     </div>
   );
 }
---
+++ src/components/ContactCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ContactCard() {
+    return (
+        <div className="contact-card">
+            <img src="/"/>
+            <h3>Name</h3>
+            <p>Phone</p>
+            <p>Email</p>
+        </div>
+    );
+}
+
+export default ContactCard;
